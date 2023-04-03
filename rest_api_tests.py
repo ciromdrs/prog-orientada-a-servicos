9439cac31400c88c20650abef2f00a5c9b99ac23
Ciro <ciromoraismedeiros@gmail.com>
2023-04-03 10:41:46 -0300

    Adicionar parâmetro ao script de testes É necessário informar o endereço do servidor onde a API que será testada está sendo executada.

--- rest_api_tests.py
+++ rest_api_tests.py
@@ -1,10 +1,13 @@
 '''Testes automáticos para APIs REST desenvolvidas na disciplina "Programação
 Orientada a Serviços".
 
-Requer o módulo `requests` instalado.'''
+Requer o módulo `requests` instalado.
 
+Uso: python <nome-deste-script.py> <endereco-do-servidor>
+Exemplo: python rest_api_tests.property "http://localhost:80"'''
+
+import sys
 import requests
-
 
 # Funções de teste
 def test(description: str, req_method, req_args: list, expectation,
@@ -20,7 +23,14 @@ def expect_status_code(response: requests.Response, code:int) -> (bool, str):
         return False, errmsg
     return True, ''
 
+def url(path: str) -> str:
+    '''Adiciona o endereço do servidor como prefixo a um caminho para gerar uma
+    URL.'''
+    server_address = sys.argv[1]
+    return server_address + path
+
 
 # Testes
-# GET /
-test('GET /', requests.get, ['http://localhost'], expect_status_code, [200])
+# GET /hello-world
+test('GET /hello-world', requests.get, [url('/hello-world')],
+    expect_status_code, [200])
