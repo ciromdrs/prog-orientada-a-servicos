e98db79324ddcf4fdb0022347ba1aeed8d3a20a0
Ciro <ciromoraismedeiros@gmail.com>
2023-04-03 09:29:18 -0300

    Adicionar docs de tipos de parâmetros

--- rest_api_tests.py
+++ rest_api_tests.py
@@ -7,7 +7,8 @@ import requests
 
 
 # Funções de teste
-def test(description, req_method, req_args, expectation, exp_args):
+def test(description: str, req_method: function, req_args: list,
+        expectation: function, exp_args:list):
     res, errmsg = expectation(req_method(*req_args), *exp_args)
     if not res:
         print('Erro no teste', description)
