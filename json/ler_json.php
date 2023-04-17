b154127e85aa73900554ca4672e84adbd98052da
Ciro <ciromoraismedeiros@gmail.com>
2023-04-17 16:20:57 -0300

    Omitir parâmetro depth (obrigado, Mosiah)

--- json/ler_json.php
+++ json/ler_json.php
@@ -13,12 +13,6 @@ $conteudo = file_get_contents($argv[1]);
 $meu_array = json_decode($conteudo,
                          $associative = true, // Retorna um array em vez de um
                                               // objeto padrão.
-                         $depth = 512, // Tenho que passar esse parâmetro pra
-                                       // poder passar o próximo parâmetro
-                                       // (flags). O valor default já é 512,
-                                       // então apenas repeti.
-                                       // Se você souber outra forma de fazer
-                                       // isso, me avise :)
-                         $flags = JSON_THROW_ON_ERROR); // Lança exceções em
+                         flags : JSON_THROW_ON_ERROR); // Lança exceções em
                                                         // caso de erro.
 var_dump($meu_array); // var_dump exibe qualquer variável.
