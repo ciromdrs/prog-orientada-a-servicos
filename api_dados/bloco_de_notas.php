c9cd135cdb2e48c6abb05a3a43254878b2a9bf64
Ciro <ciromoraismedeiros@gmail.com>
2023-05-26 14:48:52 -0300

    Implementar exibição de mensagens de erro e corrigir API

--- api_dados/bloco_de_notas.php
+++ api_dados/bloco_de_notas.php
@@ -13,14 +13,15 @@ if (sizeof($argv) != 2) {
 
 
 // Opções de menus
-const OPCAO_SAIR = 0;
-const OPCAO_CRIAR = 1;
-const OPCAO_APAGAR = 2;
+const OPCAO_SAIR = '0';
+const OPCAO_CRIAR = '1';
+const OPCAO_APAGAR = '2';
 
 // URL do serviço de dados
 $url_servico_dados = $argv[1];
 
-
+// Mensagem para o usuário
+$mensagem = '';
 
 // INICIALIZAÇÃO
 
@@ -68,47 +69,65 @@ while($opcao != OPCAO_SAIR);
 
 
 function menu_principal() {
-    global $urls;
+    global $urls, $mensagem;
 
     // Acessa as notas cadastradas
     $notas = req_GET_notas();
 
     // Exibe a interface
-    echo "\n--------------- BLOCO DE NOTAS ---------------\n";
+    echo "\n=============== BLOCO DE NOTAS ===============\n";
     if ($notas != NULL) {
         foreach($notas as $n){
             echo "Nota {$n->chave}: \"{$n->valor}\"\n";
         }
     }
     echo "----------------------------------------------\n";
+    if (strlen($mensagem) > 0) {
+        echo "$mensagem\n";
+        echo "----------------------------------------------\n";
+    }
+    $mensagem = ''; // Apaga a mensagem para a próxima iteração
     echo OPCAO_CRIAR . " - Criar nota\n";
     echo OPCAO_APAGAR . " - Apagar nota\n";
     echo OPCAO_SAIR . " - Sair\n";
     echo "Digite sua opção: ";
-    $opcao = (int)readline();
+    $opcao = readline();
     return $opcao;
 }
 
 
 function menu_criar() {
+    global $mensagem;
+
     // Recebe o texto da nota
     echo "Digite o texto da nota:\n";
     $texto = readline();
 
     // Envia a requisição ao serviço de dados
     $resp = req_POST_nota($texto);
-     // TODO: Exibir possível mensagem de erro
+
+    // Exibe possível mensagem de erro
+    if ($resp['codigo'] != 201) {
+        $mensagem = "Erro {$resp['codigo']} ao criar nota.\n" . $resp["corpo"];
+    }
 }
 
 
 function menu_apagar() {
+    global $mensagem;
+
     // Recebe a chave da nota
     echo "Digite o número da nota que deseja apagar: ";
     $chave = readline();
 
     // Envia a requisição DELETE ao serviço de dados
     $resp = req_DELETE_nota($chave);
-    // TODO: Exibir possível mensagem de erro
+
+    // Exibe possível mensagem de erro
+    if ($resp['codigo'] != 200) {
+        $mensagem = "Erro {$resp['codigo']} ao apagar Nota $chave.\n" .
+                    $resp["corpo"];
+    }
 }
 
 
@@ -118,36 +137,41 @@ function menu_apagar() {
 /**
  * Função genérica para enviar requisições usando a extensão cURL para PHP.
  */
-function enviar_requisicao($url, $curl_options = []) {
-    // Cria array para guardar as informações da resposta
-    $resposta = [];
+ function enviar_requisicao($url, $curl_options = []) {
+     // Cria array para guardar as informações da resposta
+     $resposta = [];
 
-    // Inicializa o canal de comunicação
-    $ch = curl_init($url);
-    // Esta opção configura o cURL para retornar o valor da resposta, em vez de
-    // apenas exibí-lo na tela.
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-    // Atribui as demais opções passadas como parâmetro.
-    foreach($curl_options as [$opt, $val]){
-        curl_setopt($ch, $opt, $val);
-    }
+     // Inicializa o canal de comunicação
+     $ch = curl_init($url);
+     // Esta opção configura o cURL para retornar o valor da resposta, em vez de
+     // apenas exibí-lo na tela.
+     curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+     // Esta opção configura o cURL para preservar os cabeçalhos na resposta, em
+     // vez de descartá-los.
+     curl_setopt($ch, CURLOPT_HEADER, 1);
+     // Atribui as demais opções passadas como parâmetro.
+     foreach($curl_options as [$opt, $val]){
+         curl_setopt($ch, $opt, $val);
+     }
 
-    // Envia a requisição HTTP e retorna o corpo da resposta HTTP
-    $resposta['corpo'] = curl_exec($ch);
-    // Extrai os cabecalhos da resposta HTTP
-    $resposta['cabecalhos'] = 'Não implementado';
-    // Acessa informações sobre a resposta
-    $resposta['info'] = curl_getinfo($ch);
-    // Extrai o código de estado HTTP
-    $resposta['codigo'] = $resposta['info']['http_code'];
-    // Acessa possível erro na requisição
-    $resposta['erro'] = curl_error($ch);
-    // Fecha o canal de comunicação
-    curl_close($ch);
+     // Envia a requisição HTTP e retorna o corpo da resposta HTTP
+     $str_resp = curl_exec($ch);
+     // Acessa informações sobre a resposta
+     $info = curl_getinfo($ch);
+     // Extrai o código de estado HTTP
+     $resposta['codigo'] = $info['http_code'];
+     // Extrai os cabecalhos da resposta HTTP
+     $resposta['cabecalhos'] = substr($str_resp, 0, $info['header_size']);
+     // Extrai o corpo da resposta HTTP
+     $resposta['corpo'] = substr($str_resp, $info['header_size']);
+     // Extrai possível erro na requisição
+     $resposta['erro'] = curl_error($ch);
+     // Fecha o canal de comunicação
+     curl_close($ch);
 
-    // Retorna a resposta e as informações
-    return $resposta;
-}
+     // Retorna a resposta e as informações
+     return $resposta;
+ }
 
 
 function req_GET_notas() {
