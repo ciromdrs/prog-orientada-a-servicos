8188e884f8adcfdaad410e6d37c030ca786ecadb
Ciro <ciromoraismedeiros@gmail.com>
2023-06-02 17:06:07 -0300

    Melhorar função enviar_requisicao

--- a4-requisicoes/requisicoes.php
+++ a4-requisicoes/requisicoes.php
@@ -6,8 +6,8 @@ $url_api = 'localhost:8000/api';
  * Função genérica para enviar requisições usando a extensão cURL para PHP.
  */
 function enviar_requisicao($url, $metodo = 'GET', $corpo = '',
-    $curl_options = []) {
-    // Cria array para guardar as informações da resposta
+    $cabecalhos = [], $curl_options = []) {
+    // Cria um array para guardar as informações da resposta
     $resposta = [];
 
     // Inicializa o canal de comunicação
@@ -22,14 +22,14 @@ function enviar_requisicao($url, $metodo = 'GET', $corpo = '',
     // Esta opção configura o método HTTP. Se ela não for atribuída, o método
     // GET é usado por padrão.
     curl_setopt($ch, CURLOPT_CUSTOMREQUEST, $metodo);
+    // Esta opção atribui os cabecalhos da requisição.
+    curl_setopt($ch, CURLOPT_HTTPHEADER, $cabecalhos);
     // Esta opção atribui um corpo à requisição HTTP. Em geral, o corpo é usado
     // por requisições POST, PUT ou PATCH, porém talvez o mais comum seja POST
     // (por isso a opção se chama POSTFIELDS).
     curl_setopt($ch, CURLOPT_POSTFIELDS, $corpo);
     // Atribui as demais opções passadas como parâmetro.
-    foreach($curl_options as [$opt, $val]){
-        curl_setopt($ch, $opt, $val);
-    }
+    curl_setopt_array($ch, $curl_options);
 
     // Envia a requisição HTTP e retorna o corpo da resposta HTTP
     $str_resp = curl_exec($ch);
@@ -37,8 +37,11 @@ function enviar_requisicao($url, $metodo = 'GET', $corpo = '',
     $info = curl_getinfo($ch);
     // Extrai o código de estado HTTP
     $resposta['codigo'] = $info['http_code'];
-    // Extrai os cabecalhos da resposta HTTP
-    $resposta['cabecalhos'] = substr($str_resp, 0, $info['header_size']);
+    // Extrai os cabecalhos da resposta HTTP (sem a linha do codigo HTTP)
+    $str_cabecalhos = substr($str_resp, 0, $info['header_size']);
+    $linhas = explode('\n', $str_cabecalhos);
+    $resposta['cabecalhos'] = array_slice($linhas, 1);
+
     // Extrai o corpo da resposta HTTP
     $resposta['corpo'] = substr($str_resp, $info['header_size']);
     // Extrai possível erro na requisição
