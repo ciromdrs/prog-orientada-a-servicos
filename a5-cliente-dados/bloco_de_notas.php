e345ce39602cb10fd6fac5004b093a86ca810749
Ciro <ciromoraismedeiros@gmail.com>
2023-06-21 15:53:20 -0300

    Atualizar função enviar_requisicao no bloco de notas da A5

--- a5-cliente-dados/bloco_de_notas.php
+++ a5-cliente-dados/bloco_de_notas.php
@@ -137,42 +137,53 @@ function menu_apagar() {
 /**
  * Função genérica para enviar requisições usando a extensão cURL para PHP.
  */
- function enviar_requisicao($url, $curl_options = []) {
-     // Cria array para guardar as informações da resposta
-     $resposta = [];
+function enviar_requisicao($url, $metodo = 'GET', $corpo = '',
+    $cabecalhos = [], $curl_options = []) {
+    // Cria um array para guardar as informações da resposta
+    $resposta = [];
 
-     // Inicializa o canal de comunicação
-     $ch = curl_init($url);
-     // Esta opção configura o cURL para retornar o valor da resposta, em vez de
-     // apenas exibí-lo na tela.
-     curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-     // Esta opção configura o cURL para preservar os cabeçalhos na resposta, em
-     // vez de descartá-los.
-     curl_setopt($ch, CURLOPT_HEADER, 1);
-     // Atribui as demais opções passadas como parâmetro.
-     foreach($curl_options as [$opt, $val]){
-         curl_setopt($ch, $opt, $val);
-     }
+    // Inicializa o canal de comunicação
+    $ch = curl_init($url);
 
-     // Envia a requisição HTTP e retorna o corpo da resposta HTTP
-     $str_resp = curl_exec($ch);
-     // Acessa informações sobre a resposta
-     $info = curl_getinfo($ch);
-     // Extrai o código de estado HTTP
-     $resposta['codigo'] = $info['http_code'];
-     // Extrai os cabecalhos da resposta HTTP
-     $resposta['cabecalhos'] = substr($str_resp, 0, $info['header_size']);
-     // Extrai o corpo da resposta HTTP
-     $resposta['corpo'] = substr($str_resp, $info['header_size']);
-     // Extrai possível erro na requisição
-     $resposta['erro'] = curl_error($ch);
-     // Fecha o canal de comunicação
-     curl_close($ch);
+    // Esta opção configura o cURL para retornar o valor da resposta, em vez de
+    // apenas exibí-lo na tela.
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    // Esta opção configura o cURL para preservar os cabeçalhos na resposta, em
+    // vez de descartá-los.
+    curl_setopt($ch, CURLOPT_HEADER, 1);
+    // Esta opção configura o método HTTP. Se ela não for atribuída, o método
+    // GET é usado por padrão.
+    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, $metodo);
+    // Esta opção atribui os cabecalhos da requisição.
+    curl_setopt($ch, CURLOPT_HTTPHEADER, $cabecalhos);
+    // Esta opção atribui um corpo à requisição HTTP. Em geral, o corpo é usado
+    // por requisições POST, PUT ou PATCH, porém talvez o mais comum seja POST
+    // (por isso a opção se chama POSTFIELDS).
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $corpo);
+    // Atribui as demais opções passadas como parâmetro.
+    curl_setopt_array($ch, $curl_options);
 
-     // Retorna a resposta e as informações
-     return $resposta;
- }
+    // Envia a requisição HTTP e retorna o corpo da resposta HTTP
+    $str_resp = curl_exec($ch);
+    // Acessa informações sobre a resposta
+    $info = curl_getinfo($ch);
+    // Extrai o código de estado HTTP
+    $resposta['codigo'] = $info['http_code'];
+    // Extrai os cabecalhos da resposta HTTP (sem a linha do codigo HTTP)
+    $str_cabecalhos = substr($str_resp, 0, $info['header_size']);
+    $linhas = explode('\n', $str_cabecalhos);
+    $resposta['cabecalhos'] = array_slice($linhas, 1);
 
+    // Extrai o corpo da resposta HTTP
+    $resposta['corpo'] = substr($str_resp, $info['header_size']);
+    // Extrai possível erro na requisição
+    $resposta['erro'] = curl_error($ch);
+    // Fecha o canal de comunicação
+    curl_close($ch);
+
+    // Retorna a resposta
+    return $resposta;
+}
 
 function req_GET_notas() {
     global $urls;
@@ -204,7 +215,7 @@ function req_GET_urls($url_servico) {
 function req_DELETE_nota($chave) {
     return enviar_requisicao(
         _url('objeto', [['{balde}','notas'], ['{chave}', $chave]]),
-        [[CURLOPT_CUSTOMREQUEST, 'DELETE']]
+        metodo: 'DELETE'
     );
 }
 
@@ -212,23 +223,20 @@ function req_DELETE_nota($chave) {
 function req_HEAD_balde_notas() {
     return enviar_requisicao(
         _url('balde', [['{balde}', 'notas']]),
-        [[CURLOPT_CUSTOMREQUEST, 'HEAD']]
-    );
+        metodo: 'HEAD'
+        );
 }
 
 
 function req_PUT_balde_notas() {
     return enviar_requisicao(
         _url('balde', [['{balde}', 'notas']]),
-        [
-            [CURLOPT_CUSTOMREQUEST, 'PUT'],
-            [CURLOPT_HTTPHEADER, array('Content-Type: application/json')],
-            [CURLOPT_POSTFIELDS, json_encode([
-                    'usuario' => 'bloco_de_notas',
-                    'nome' => 'notas'
-                ])
-            ],
-        ]
+        metodo: 'PUT',
+        cabecalhos: ['Content-Type: application/json'],
+        corpo: json_encode([
+            'usuario' => 'bloco_de_notas',
+            'nome' => 'notas'
+        ])
     );
 }
 
@@ -236,15 +244,12 @@ function req_PUT_balde_notas() {
 function req_POST_nota($texto) {
     return enviar_requisicao(
         _url('balde', [['{balde}', 'notas']]),
-        [
-            [CURLOPT_CUSTOMREQUEST, 'POST'],
-            [CURLOPT_HTTPHEADER, array('Content-Type: application/json')],
-            [CURLOPT_POSTFIELDS, json_encode([
-                    'usuario' => 'bloco_de_notas',
-                    'valor' => $texto
-                ])
-            ]
-        ]
+        metodo: 'POST',
+        cabecalhos: ['Content-Type: application/json'],
+        corpo: json_encode([
+            'usuario' => 'bloco_de_notas',
+            'valor' => $texto
+        ])
     );
 }
 
