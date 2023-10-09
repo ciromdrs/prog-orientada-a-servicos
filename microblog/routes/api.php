a9cbd896eff2686a5404a2b24a4b526351659e32
Ciro <ciromoraismedeiros@gmail.com>
2023-10-09 12:40:56 -0300

    Adicionar resposta exemplo para o GET /publicacoes

--- microblog/routes/api.php
+++ microblog/routes/api.php
@@ -18,3 +18,25 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+
+Route::get('publicacoes',
+    function(Request $request) {
+        # Apenas um exemplo de resposta. Os dados deveriam vir do banco.
+        return response()->json([
+            [
+                'id' => 1,
+                'autor' => 'alice',
+                'texto' => 'Minha primeira publicação.',
+                'criacao' => '2023-10-07T15:30:00',
+            ],
+            [
+                'id' => 2,
+                'autor' => 'bob',
+                'texto' => 'Publicação exemplo.',
+                'criacao' => '2023-10-08T07:30:00',
+            ],
+        ]);
+    }
+);
+
+
