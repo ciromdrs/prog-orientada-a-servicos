7e2d41ca63c8ce207f34f9c2dd80d1200240c4d5
Ciro <ciromoraismedeiros@gmail.com>
2023-11-16 14:04:21 -0300

    Implementar PublicacaoController

--- microblog/servico/app/Http/Controllers/PublicacaoController.php
+++ microblog/servico/app/Http/Controllers/PublicacaoController.php
@@ -2,40 +2,89 @@
 
 namespace App\Http\Controllers;
 
+
+use App\Models\Publicacao;
 use Illuminate\Http\Request;
 
 class PublicacaoController extends Controller
 {
     public function index() {
-        # Apenas um exemplo de resposta. Os dados deveriam vir do banco.
-        return response()->json([
-            [
-                'id' => 1,
-                'autor' => 'alice',
-                'texto' => 'Minha primeira publicação.',
-                'criacao' => '2023-10-07T15:30:00',
-            ],
-            [
-                'id' => 2,
-                'autor' => 'bob',
-                'texto' => 'Publicação exemplo.',
-                'criacao' => '2023-10-08T07:30:00',
-            ]
-        ]);
+        return response()->json(Publicacao::all());
     }
+
 
     public function store(Request $request) {
         $request->validate([
             'autor' => 'required',
             'texto' => 'required|max:144',
         ]);
-        # Apenas um exemplo de resposta. Os dados deveriam ser salvos no banco.
+        Publicacao::create($request->all());
+        
         return response()->json(
             [
                 'tipo' => 'info',
-                'conteudo' => "Publicação criada (mentira, falta implementar isso)."
+                'conteudo' => "Publicação criada."
             ],
             201
         );
+    }
+
+
+    public function show(Request $request, $id) {
+        $p = Publicacao::find($id);
+
+        if (!$p) {
+            return response()->json(
+                [
+                    'tipo' => 'erro',
+                    'conteudo' => "Publicação $id não encontrada."
+                ],
+                404
+            );
+        }
+
+        return response()->json($p);
+    }
+
+
+    public function update(Request $request, $id) {
+        $request->validate([
+            'autor' => 'required',
+            'texto' => 'required|max:144',
+        ]);
+
+        $p = Publicacao::find($id);
+
+        if (!$p) {
+            return response()->json(
+                [
+                    'tipo' => 'erro',
+                    'conteudo' => "Publicação $id não encontrada."
+                ],
+                404
+            );
+        }
+
+        $p->update($request->all());
+    }
+
+    public function destroy(Request $request, $id) {
+        $p = Publicacao::find($id);
+        
+        if (!$p) {
+            return response()->json(
+                [
+                    'tipo' => 'erro',
+                    'conteudo' => "Publicação $id não encontrada."
+                ],
+                404
+            );
+        }
+
+        $p->delete();
+        return response()->json([
+            'tipo' => 'info',
+            'conteudo' => "Publicação apagada."
+        ]);
     }
 }
