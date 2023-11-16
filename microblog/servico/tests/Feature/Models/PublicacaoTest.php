52ad05123e13a6543b8fed87eac6e37809573bbf
Ciro <ciromoraismedeiros@gmail.com>
2023-11-16 13:18:34 -0300

    Criar PublicacaoTest

---
+++ microblog/servico/tests/Feature/Models/PublicacaoTest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Tests\Feature\Models;
+
+use App\Models\Publicacao;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class PublicacaoTest extends TestCase
+{
+    use RefreshDatabase;
+
+
+    /**
+     * Testar se cria uma Publicacao.
+     *
+     * @return void
+     */
+    public function test_criar()
+    {
+        Publicacao::create([
+            'autor' => 'alice',
+            'texto' => 'Publicação teste.'
+        ]);
+
+        $this->assertDatabaseHas('publicacoes',
+            ['autor' => 'alice', 'texto' => 'Publicação teste.']
+        );
+    }
+}
