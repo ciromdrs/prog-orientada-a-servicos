028b4c5cfe29c4739455d5e13656db3156733dab
Ciro <ciromoraismedeiros@gmail.com>
2023-11-16 13:03:59 -0300

    Criar migração da tabela publicacoes

---
+++ microblog/servico/database/migrations/2023_11_14_154235_create_publicacoes_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('publicacoes', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('autor');
+            $table->string('texto');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('publicacoes');
+    }
+};
