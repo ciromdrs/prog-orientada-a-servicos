93aba65db0ae4abc3c4ab4dc978a0373d75136e8
Ciro <ciromoraismedeiros@gmail.com>
2023-11-16 13:18:16 -0300

    Criar modelo Publicacao

---
+++ microblog/servico/app/Models/Publicacao.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Publicacao extends Model
+{
+    use HasFactory;
+
+    protected $table = 'publicacoes';
+
+    protected $fillable = [
+        'autor',
+        'texto'
+    ];
+}
