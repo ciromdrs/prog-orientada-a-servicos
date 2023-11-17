1e7cbd058d1e490bdfa9102f191777cf44740d18
Ciro <ciromoraismedeiros@gmail.com>
2023-11-17 07:25:37 -0300

    Adicionar script da interface do Microblog

--- microblog/cliente/principal.php
+++ microblog/cliente/principal.php
@@ -2,51 +2,21 @@
 
 require 'vendor/autoload.php';
 require 'cliente_microblog.php';
+require 'interface.php';
 
-
-/* Constantes */
-const LIMPA_TELA = "\033c"; # Imprimir esse caractere com echo limpa a tela
-const OPCAO_SAIR = 0;
 
 /* Variáveis */
 $api = new ClienteMicroblog('http://localhost:8000/api/');
-
+$interface = new InterfaceMicroblog();
 
 /* Programa principal */
 do {
-    echo LIMPA_TELA . 
-    "\r---------------------------------------------------------------------
-    \r                            MICROBLOG
-    \r---------------------------------------------------------------------
-    ";
+    $interface->exibirTitulo();
 
     $publicacoes = $api->GET_publicacoes();
-    exibir_publicacoes($publicacoes);
+    $interface->exibirPublicacoes($publicacoes);
 
-    $opcao = menu_operacoes();
-} while ($opcao != OPCAO_SAIR);
+    $opcao = $interface->menuOperacoes();
+} while ($opcao != OP_CANCELAR);
 
-
-/* Funções */
-function exibir_publicacoes($publicacoes) {
-    foreach ($publicacoes as $p) {
-        echo "
-        \r$p->autor em $p->created_at escreveu:
-        \r\"$p->texto\"
-        \r";
-    }
-}
-
-
-function menu_operacoes() {
-    echo "Operações:\n";
-    $operacoes = [
-        1 => 'Escrever publicação',
-        2 => 'Excluir publicação',
-        0 => 'Sair'
-    ];
-    foreach ($operacoes as $i => $op) {
-        echo "[$i] $op\n";
-    }
-    return readline('O que você deseja fazer? ');
-}
+$interface->tchau();
