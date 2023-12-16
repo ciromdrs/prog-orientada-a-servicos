c54c632597597a27dd3b0b81b4b6b751ad920c7c
Ciro <ciromoraismedeiros@gmail.com>
2023-12-16 09:28:01 -0300

    Adicionar experimento de busca

--- extra/src/experimentar.php
+++ extra/src/experimentar.php
@@ -22,7 +22,8 @@ $incremento = 100000;
 $ate = $incremento * 10;
 
 # Executa os experimentos
-exp_inserir($classe, 0, $ate, $incremento, 10);
+#exp_inserir($classe, 0, $ate, $incremento, 10);
+exp_buscar($classe, 0, $ate, $incremento, 10);
 
 
 /**
@@ -52,12 +53,44 @@ function exp_inserir($classe, int $de, int $ate, int $incremento, int $repeticoe
             $decorrido = agora() - $inicio;
 
             # Exibe os resultados do experimento
-            $tempo = number_format(
-                $decorrido,
-                decimals: 10,
-                decimal_separator: ",",
-                thousands_separator: ""
-            );
+            $tempo = formatar_tempo($decorrido);
+            echo "$tam\t$tempo\n";
+        }
+    }
+}
+
+
+/**
+ * Experimento de busca.
+ */
+function exp_buscar($classe, int $de, int $ate, int $incremento, int $repeticoes)
+{
+    # Identificação do experimento
+    echo "$classe.buscar\t$de\t$ate\t$incremento\t$repeticoes\n";
+
+    # Incrementa o tamanho de $de até $ate
+    for ($tam = $de; $tam <= $ate; $tam += $incremento) {
+        # Repete cada experimento $repeticoes vezes
+        for ($r = 0; $r < $repeticoes; $r++) {
+            # Cria um array de valores aleatórios
+            $dados = range(0, $tam); # Primeiro cria ordenado
+            shuffle($dados); # Depois embaralha
+
+            # Cria uma nova instância da estrutura que for passada fornecendo
+            # o array embaralhado
+            $estrutura = new $classe($dados);
+
+            # Pega a hora inicial do experimento
+            $inicio = agora();
+
+            # Busca um elemento ausente (pior caso)
+            $estrutura->inserir($tam + 1);
+
+            # Subtrai a hora inicial da final para pegar o tempo decorrido
+            $decorrido = agora() - $inicio;
+
+            # Exibe os resultados do experimento
+            $tempo = formatar_tempo($decorrido);
             echo "$tam\t$tempo\n";
         }
     }
@@ -70,4 +103,18 @@ function exp_inserir($classe, int $de, int $ate, int $incremento, int $repeticoe
 function agora()
 {
     return microtime(as_float: true);
+}
+
+
+/**
+ * Função auxiliar que formata tempo no formato esperado.
+ */
+function formatar_tempo($tempo)
+{
+    return number_format(
+        $tempo,
+        decimals: 10,
+        decimal_separator: ",",
+        thousands_separator: ""
+    );
 }
